6471391602f6f4cb5d2b7f41ff3ceb12d1b2ed9a
manojgowda593 <manoj.gowda1593@gmail.com>
2024-12-22 23:20:48 +0530

    Create test_placeholder.py

---
+++ Jenkins/test_placeholder.py
@@ -0,0 +1,9 @@
+# test_placeholder.py
+import unittest
+
+class TestPlaceholder(unittest.TestCase):
+    def test_placeholder(self):
+        self.assertEqual(1, 1)  # Always passes
+
+if __name__ == '__main__':
+    unittest.main()
